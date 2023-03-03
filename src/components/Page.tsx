723aa56d710860b842da4b3bca12fc8f0940c174
HashRei <hashrei@protonmail.com>
2023-03-03 21:18:10 +0100

    Rename section => sections

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import React, { ReactNode } from "react";
-import { Navbar } from "./section/Navbar";
+import { Navbar } from "./sections/Navbar";
 
 interface PageProps {
   children: ReactNode;
