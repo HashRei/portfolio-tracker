43582c2d268e3ac39d09f316b0ce9aa7870d4153
HashRei <hashrei@protonmail.com>
2023-03-04 13:27:41 +0100

    Update SectionGraphCardProps

--- src/components/cardGraphs/SectionGraphCard.tsx
+++ src/components/cardGraphs/SectionGraphCard.tsx
@@ -1,4 +1,4 @@
-import { Line } from "react-chartjs-2";
+import { Bar, Line } from "react-chartjs-2";
 import {
   BarElement,
   CategoryScale,
@@ -25,6 +25,10 @@ interface SectionGraphCardProps {
   currentSectionValue: number;
   currentSectionDelta: number;
   backgroundColor: string;
+  graphTension: number;
+  fillGraph: boolean;
+  graphType: string;
+  graphLabel: string
 }
 
 export function SectionGraphCard({
@@ -32,6 +36,10 @@ export function SectionGraphCard({
   currentSectionValue,
   currentSectionDelta,
   backgroundColor,
+  graphTension,
+  fillGraph,
+  graphType,
+  graphLabel,
 }: SectionGraphCardProps) {
   return (
     <div
@@ -50,67 +58,133 @@ export function SectionGraphCard({
         </div>
       </div>
       <div className="mt-3 mx-3" style={{ height: "70px" }}>
-        <Line
-          options={{
-            plugins: {
-              legend: {
-                display: false,
-              },
-            },
-            maintainAspectRatio: false,
-            scales: {
-              x: {
-                grid: {
-                  display: false,
-                },
-                ticks: {
+        {graphType === "Line" && "line" ? (
+          <Line
+            options={{
+              plugins: {
+                legend: {
                   display: false,
                 },
               },
-              y: {
-                min: 30,
-                max: 89,
-                display: false,
-                grid: {
+              maintainAspectRatio: false,
+              scales: {
+                x: {
+                  grid: {
+                    display: false,
+                  },
+                  ticks: {
+                    display: false,
+                  },
+                },
+                y: {
+                  min: 30,
+                  max: 89,
+                  display: false,
+                  grid: {
+                    display: false,
+                  },
+                  ticks: {
+                    display: false,
+                  },
+                },
+              },
+              elements: {
+                line: {
+                  borderWidth: 1,
+                  tension: graphTension,
+                },
+                point: {
+                  radius: 4,
+                  hitRadius: 10,
+                  hoverRadius: 4,
+                },
+              },
+            }}
+            data={{
+              labels: [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+              ],
+              datasets: [
+                {
+                  label: graphLabel,
+                  backgroundColor: "rgba(255,255,255,.2)",
+                  borderColor: "rgba(255,255,255,.55)",
+                  data: [65, 59, 84, 84, 51, 55, 40],
+                  fill: fillGraph,
+                },
+              ],
+            }}
+          />
+        ) : (
+          <Bar
+            options={{
+              maintainAspectRatio: false,
+              plugins: {
+                legend: {
                   display: false,
                 },
-                ticks: {
-                  display: false,
+              },
+              scales: {
+                x: {
+                  grid: {
+                    display: false,
+                    drawTicks: false,
+                  },
+                  ticks: {
+                    display: false,
+                  },
+                },
+                y: {
+                  grid: {
+                    display: false,
+                    drawTicks: false,
+                  },
+                  ticks: {
+                    display: false,
+                  },
                 },
               },
-            },
-            elements: {
-              line: {
-                borderWidth: 1,
-                tension: 0.4,
-              },
-              point: {
-                radius: 4,
-                hitRadius: 10,
-                hoverRadius: 4,
-              },
-            },
-          }}
-          data={{
-            labels: [
-              "January",
-              "February",
-              "March",
-              "April",
-              "May",
-              "June",
-              "July",
-            ],
-            datasets: [
-              {
-                label: "My First dataset",
-                backgroundColor: "transparent",
-                borderColor: "rgba(255,255,255,.55)",
-                data: [65, 59, 84, 84, 51, 55, 40],
-              },
-            ],
-          }}
-        />
+            }}
+            data={{
+              labels: [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December",
+                "January",
+                "February",
+                "March",
+                "April",
+              ],
+              datasets: [
+                {
+                  label: graphLabel,
+                  backgroundColor: "rgba(255,255,255,.5)",
+                  borderColor: "rgba(255,255,255,.55)",
+                  data: [
+                    78, 81, 80, 45, 34, 12, 40, 85, 65, 23, 12, 98, 34, 84, 67,
+                    82,
+                  ],
+                  barPercentage: 0.6,
+                },
+              ],
+            }}
+          />
+        )}
       </div>
     </div>
   );
--- src/components/cardGraphs/SectionGraphCardGallery.tsx
+++ src/components/cardGraphs/SectionGraphCardGallery.tsx
@@ -9,12 +9,20 @@ export function SectionGraphCardGallery() {
           currentSectionValue={46}
           currentSectionDelta={8}
           backgroundColor="rgb(0, 123, 255)"
+          graphTension={0.4}
+          fillGraph={false}
+          graphType="Line"
+          graphLabel="graphLabel"
         />
         <SectionGraphCard
           sectionName="Stocks"
           currentSectionValue={46}
           currentSectionDelta={14}
           backgroundColor="#17a2b8"
+          graphTension={0.4}
+          fillGraph={true}
+          graphType="Line"
+          graphLabel="graphLabel"
         />
       </div>
       <div className="flex justify-center">
@@ -23,304 +31,22 @@ export function SectionGraphCardGallery() {
           currentSectionValue={46}
           currentSectionDelta={26}
           backgroundColor="#28a745"
+          graphTension={0}
+          fillGraph={true}
+          graphType="Line"
+          graphLabel="graphLabel"
         />
         <SectionGraphCard
           sectionName="Real Estate"
           currentSectionValue={46}
           currentSectionDelta={3}
           backgroundColor="#ffc107"
+          graphTension={0.4}
+          fillGraph={false}
+          graphType="Bar"
+          graphLabel="graphLabel"
         />
       </div>
-      {/* <div className="flex justify-center">
-        <div className="sm:w-1/2 lg:w-1/4 rounded-xl p-2 m-2 bg-blue-600 ">
-          <div className="pb-0 flex justify-between items-start mb-4">
-            <div>
-              <div className="text-lg font-semibold">
-                26K
-                <span className="text-sm ml-2 font-normal">(-12.4%)</span>
-              </div>
-              <div>Gold & Silver</div>
-            </div>
-          </div>
-          <div className="mt-3 mx-3" style={{ height: "70px" }}>
-            <Line
-              options={{
-                plugins: {
-                  legend: {
-                    display: false,
-                  },
-                },
-                maintainAspectRatio: false,
-                scales: {
-                  x: {
-                    grid: {
-                      display: false,
-                    },
-                    ticks: {
-                      display: false,
-                    },
-                  },
-                  y: {
-                    min: 30,
-                    max: 89,
-                    display: false,
-                    grid: {
-                      display: false,
-                    },
-                    ticks: {
-                      display: false,
-                    },
-                  },
-                },
-                elements: {
-                  line: {
-                    borderWidth: 1,
-                    tension: 0.4,
-                  },
-                  point: {
-                    radius: 4,
-                    hitRadius: 10,
-                    hoverRadius: 4,
-                  },
-                },
-              }}
-              data={{
-                labels: [
-                  "January",
-                  "February",
-                  "March",
-                  "April",
-                  "May",
-                  "June",
-                  "July",
-                ],
-                datasets: [
-                  {
-                    label: "My First dataset",
-                    backgroundColor: "transparent",
-                    borderColor: "rgba(255,255,255,.55)",
-                    data: [65, 59, 84, 84, 51, 55, 40],
-                  },
-                ],
-              }}
-            />
-          </div>
-        </div>
-        <div className="sm:w-1/2 lg:w-1/4 rounded-xl p-2 m-2 bg-[#17a2b8]">
-          <div className="pb-0 flex justify-between items-start mb-4">
-            <div>
-              <div className="text-lg font-semibold">
-                26K
-                <span className="text-sm ml-2 font-normal">(-12.4%)</span>
-              </div>
-              <div>Stocks</div>
-            </div>
-          </div>
-          <div className="mt-3 mx-3" style={{ height: "70px" }}>
-            <Line
-              options={{
-                plugins: {
-                  legend: {
-                    display: false,
-                  },
-                },
-                maintainAspectRatio: false,
-                scales: {
-                  x: {
-                    display: false,
-                  },
-                  y: {
-                    display: false,
-                  },
-                },
-                elements: {
-                  line: {
-                    borderWidth: 2,
-                    tension: 0.4,
-                  },
-                  point: {
-                    radius: 0,
-                    hitRadius: 10,
-                    hoverRadius: 4,
-                  },
-                },
-              }}
-              data={{
-                labels: [
-                  "January",
-                  "February",
-                  "March",
-                  "April",
-                  "May",
-                  "June",
-                  "July",
-                ],
-                datasets: [
-                  {
-                    label: "My First dataset",
-                    backgroundColor: "rgba(255,255,255,.2)",
-                    borderColor: "rgba(255,255,255,.55)",
-                    data: [78, 81, 80, 45, 34, 12, 40],
-                    fill: true,
-                  },
-                ],
-              }}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="flex justify-center">
-        <div className="sm:w-1/2 lg:w-1/4 rounded-xl p-2 m-2 bg-[#28a745] ">
-          <div className="pb-0 flex justify-between items-start mb-4">
-            <div>
-              <div className="text-lg font-semibold">
-                26K
-                <span className="text-sm ml-2 font-normal">
-                  (-12.4%)
-                </span>
-              </div>
-              <div>Crypto</div>
-            </div>
-          </div>
-          <div className="mt-3 mx-3" style={{ height: "70px" }}>
-            <Line
-              options={{
-                plugins: {
-                  legend: {
-                    display: false,
-                  },
-                },
-                maintainAspectRatio: false,
-                scales: {
-                  x: {
-                    grid: {
-                      display: false,
-                    },
-                    ticks: {
-                      display: false,
-                    },
-                  },
-                  y: {
-                    min: -9,
-                    max: 39,
-                    display: false,
-                    grid: {
-                      display: false,
-                    },
-                    ticks: {
-                      display: false,
-                    },
-                  },
-                },
-                elements: {
-                  line: {
-                    borderWidth: 1,
-                  },
-                  point: {
-                    radius: 4,
-                    hitRadius: 10,
-                    hoverRadius: 4,
-                  },
-                },
-              }}
-              data={{
-                labels: [
-                  "January",
-                  "February",
-                  "March",
-                  "April",
-                  "May",
-                  "June",
-                  "July",
-                ],
-                datasets: [
-                  {
-                    label: "My First dataset",
-                    backgroundColor: "transparent",
-                    borderColor: "rgba(255,255,255,.55)",
-                    data: [1, 18, 9, 17, 34, 22, 11],
-                  },
-                ],
-              }}
-            />
-          </div>
-        </div>
-        <div className="sm:w-1/2 lg:w-1/4 rounded-xl p-2 m-2 bg-[#ffc107] ">
-          <div className="pb-0 flex justify-between items-start mb-4">
-            <div>
-              <div className="text-lg font-semibold">
-                26K
-                <span className="text-sm ml-2 font-normal">(-12.4%)</span>
-              </div>
-              <div>Real Estate</div>
-            </div>
-          </div>
-          <div className="mt-3 mx-3" style={{ height: "70px" }}>
-            <Bar
-              options={{
-                maintainAspectRatio: false,
-                plugins: {
-                  legend: {
-                    display: false,
-                  },
-                },
-                scales: {
-                  x: {
-                    grid: {
-                      display: false,
-                      drawTicks: false,
-                    },
-                    ticks: {
-                      display: false,
-                    },
-                  },
-                  y: {
-                    grid: {
-                      display: false,
-                      drawTicks: false,
-                    },
-                    ticks: {
-                      display: false,
-                    },
-                  },
-                },
-              }}
-              data={{
-                labels: [
-                  "January",
-                  "February",
-                  "March",
-                  "April",
-                  "May",
-                  "June",
-                  "July",
-                  "August",
-                  "September",
-                  "October",
-                  "November",
-                  "December",
-                  "January",
-                  "February",
-                  "March",
-                  "April",
-                ],
-                datasets: [
-                  {
-                    label: "My First dataset",
-                    backgroundColor: "rgba(255,255,255,0.4)",
-                    borderColor: "rgba(255,255,255,.55)",
-                    data: [
-                      78, 81, 80, 45, 34, 12, 40, 85, 65, 23, 12, 98, 34, 84,
-                      67, 82,
-                    ],
-                    barPercentage: 0.6,
-                  },
-                ],
-              }}
-            />
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
